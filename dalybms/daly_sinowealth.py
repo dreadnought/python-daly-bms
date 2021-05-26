160221d0ff3abe932d57e0c90132986423aad50e
Patrick Salecker <mail@salecker.org>
2021-05-26 19:52:01 +0200

    Implement more values of the Sinowealth protocol

--- dalybms/daly_sinowealth.py
+++ dalybms/daly_sinowealth.py
@@ -2,8 +2,31 @@ import serial
 import struct
 import logging
 
+"""
+List from BMStool PC / Sinowealth
+1 = Cell 1 Voltage
+....
+09 = Cell 9 Voltage
+0A = Cell 10 Voltage
+0B = Total Voltage
+0C = External Temperature 1
+0D = External Temperature 2
+0E = IC Temperature 1
+0F = IC Temperature 2
+10 = CADC Current (4 byte)
+11 = Full Charge Capacity (4 byte)
+12 = Remaining Capacity (4 byte)
+13 = RSOC
+14 = Cycle Count
+15 = Pack Status
+16 = Battery Status
+17 = Pack Config
+18 = Manufacture Access
+"""
+
+
 class DalyBMSSinowealth:
-    def __init__(self, request_retries=3,  logger=None):
+    def __init__(self, request_retries=3, logger=None):
         """
 
         :param request_retries: How often read requests should get repeated in case that they fail (Default: 3).
@@ -51,16 +74,23 @@ class DalyBMSSinowealth:
             self.logger.error("serial write failed for command" % command)
             return False
 
-        response_data = self.serial.read(3)
+        if command in ("10", "11", "12"):
+            length = 5
+            ctype = "I"
+        else:
+            length = 3
+            ctype = "H"
+
+        response_data = self.serial.read(length)
         if len(response_data) == 0:
             self.logger.debug("empty response for command %s" % (command))
             return False
 
         self.logger.debug("%s %i" % (response_data.hex(), len(response_data)))
-        return struct.unpack('>H x', response_data)[0]
+        return struct.unpack('>%s x' % ctype, response_data)[0]
 
     def get_cell_voltages(self):
-        max_cells = 11
+        max_cells = 10
         x = 1
         cell_voltages = {}
         while x < max_cells:
@@ -72,21 +102,37 @@ class DalyBMSSinowealth:
                 break
 
             cell_voltages[x] = response_data / 1000
-
             x += 1
 
         return cell_voltages
 
+    def _read_bulk(self, requests):
+        data = {}
+        for key, command in requests.items():
+            response_data = self._read(command)
+            if not response_data:
+                continue
+            data[key] = response_data / 1000
+
+        return data
+
     def get_soc(self):
-        response_data = self._read("b")
-        if not response_data:
-            return False
-        return {
-            "total_voltage": response_data / 1000
+        requests = {
+            "total_voltage": "b",
+            "current": "10",
+            "soc_percent": "13"
         }
+        return self._read_bulk(requests)
+
+
+    def get_temperatures(self):
+        requests = {
+            "external1": "c",
+            "external2": "d",
+            "ic1": "e",
+            "ic2": "f",
+        }
+        return self._read_bulk(requests)
 
     def get_status(self):
         return {}
-
-
-
